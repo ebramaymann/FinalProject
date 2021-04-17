2a8e852b56f0d84231218c0b5c0bf518a3477227
ebramaymann <44176481+ebramaymann@users.noreply.github.com>
2021-04-17 17:06:37 +0200

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GraduationProjectIdentity.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<GraduationProjectIdentity.Models.Good> Good { get; set; }
+        public DbSet<GraduationProjectIdentity.Models.TypeGood> typeGoods { get; set; }
+        public DbSet<GraduationProjectIdentity.Models.Offer> Offers { get; set; }
+
+    }
+}
---
+++ WebApplication1/Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GraduationProjectIdentity.Models
+{
+    public partial class Role
+    {
+        //public TypeRegister()
+        //{
+        //    Registers = new HashSet<Register>();
+        //}
+
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        //public virtual ICollection<Register> Registers { get; set; }
+    }
+}
